49d2b9a4bbc29d1b176b619e293b1961682f1a7c
GaryQ-physics <garyquaresima@gmail.com>
2020-03-25 10:17:17 -0400

    use pathpatch

--- events/cut_plane.py
+++ events/cut_plane.py
@@ -23,7 +23,7 @@ import matplotlib.pyplot as plt
 # will warn that not used
 from mpl_toolkits import mplot3d
 from scipy.integrate import odeint
-from matplotlib.patches import Circle, PathPatch
+from matplotlib.patches import Circle, PathPatch, Rectangle
 from matplotlib.text import TextPath
 from matplotlib.transforms import Affine2D
 # This import registers the 3D projection, but is otherwise unused.
@@ -57,6 +57,7 @@ s = minn/60.
 
 # run parameters
 debug = False
+usePatch = True
 Nb = 10
 sign=-1  # changes sign of magnetic field used to trace the field lines
 n=50 # number of pts on cutplane grid 
@@ -304,20 +305,89 @@ x_night = np.outer(np.ones(np.size(u)), np.cos(v))
 
 #plot the planes in the 3D plot
 from matplotlib import cm as cmap
-ax1.plot_surface(X_slice, Y_slice, Z_slice, color='g', rstride=1, cstride=1,
-                       linewidth=0, antialiased=False)
-ax1.plot_surface(X_o, Y_o, Z_o, color='b', rstride=1, cstride=1,
-                       linewidth=0, antialiased=False)
+if usePatch == False:
+	ax1.plot_surface(X_slice, Y_slice, Z_slice, color='g', rstride=1, cstride=1,
+						   linewidth=0, antialiased=False)
+	ax1.plot_surface(X_o, Y_o, Z_o, color='b', rstride=1, cstride=1,
+						   linewidth=0, antialiased=False)
 ax1.plot_surface(x_day, y_day, z_day, color='w', rstride=1, cstride=1,
                        linewidth=0, antialiased=False)
 ax1.plot_surface(x_night, y_night, z_night, color='black', rstride=1, cstride=1,
                        linewidth=0, antialiased=False)
 #ax.set(xlabel='x', ylabel='y', zlabel='z')
 #p = Circle((1, 1), 1)
-#p = Rectangle((0,0), 2., 4.)
-#ax1.add_patch(p)
-#art3d.pathpatch_2d_to_3d(p, z=-4, zdir="y")
+
+# functions from stackoverflow.com by Ben Jackson 
+def rotation_matrix(d):
+    """
+    Calculates a rotation matrix given a vector d. The direction of d
+    corresponds to the rotation axis. The length of d corresponds to 
+    the sin of the angle of rotation.
+
+    Variant of: http://mail.scipy.org/pipermail/numpy-discussion/2009-March/040806.html
+    """
+    sin_angle = np.linalg.norm(d)
+
+    if sin_angle == 0:
+        return np.identity(3)
+
+    d /= sin_angle
+
+    eye = np.eye(3)
+    ddt = np.outer(d, d)
+    skew = np.array([[    0,  d[2],  -d[1]],
+                  [-d[2],     0,  d[0]],
+                  [d[1], -d[0],    0]], dtype=np.float64)
+
+    M = ddt + np.sqrt(1 - sin_angle**2) * (eye - ddt) + sin_angle * skew
+    return M
+
+def pathpatch_2d_to_3d(pathpatch, z = 0, normal = 'z'):
+    """
+    Transforms a 2D Patch to a 3D patch using the given normal vector.
+
+    The patch is projected into they XY plane, rotated about the origin
+    and finally translated by z.
+    """
+    if type(normal) is str: #Translate strings to normal vectors
+        index = "xyz".index(normal)
+        normal = np.roll((1.0,0,0), index)
+
+    normal = (1./np.linalg.norm(normal))*normal #Make sure the vector is normalised
+
+    path = pathpatch.get_path() #Get the path and the associated transform
+    trans = pathpatch.get_patch_transform()
+
+    path = trans.transform_path(path) #Apply the transform
+
+    pathpatch.__class__ = art3d.PathPatch3D #Change the class
+    pathpatch._code3d = path.codes #Copy the codes
+    pathpatch._facecolor3d = pathpatch.get_facecolor #Get the face color    
+
+    verts = path.vertices #Get the vertices in 2D
+
+    d = np.cross(normal, (0, 0, 1)) #Obtain the rotation vector   
+    M = rotation_matrix(d) #Get the rotation matrix
+
+    pathpatch._segment3d = np.array([np.dot(M, (x, y, 0)) + (0, 0, z) for x, y in verts])
+
+def pathpatch_translate(pathpatch, delta):
+    """
+    Translates the 3D pathpatch by the amount delta.
+    """
+    pathpatch._segment3d += delta
+
+if usePatch:
+	p = Rectangle((-2, -4), 4, 8, color='b')
+	q = Rectangle((-2, -4), 4, 8, color='g')
+	ax1.add_patch(p)
+	ax1.add_patch(q)
+	n=np.array([0,1,0])
+	m=U3
+	pathpatch_2d_to_3d(p, 0., n)
+	pathpatch_2d_to_3d(q, 0., m)
+	pathpatch_translate(p, v1)
+	pathpatch_translate(q, v1)
 
 
 plt.show()
-
