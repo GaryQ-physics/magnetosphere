06e2ba5da951b43de92d3e1933d81d77c853e6a4
Robert Weigel <rweigel@gmu.edu>
2020-05-26 14:10:59 -0400

    test for pos_sun

--- events/pos_sun.py
+++ events/pos_sun.py
@@ -1,31 +1,36 @@
-# pos_sun
+# Run script with no arguments to see test results
 
 import sys
 import os
 import numpy as np
+
+# Add path of config_paths.py
 sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '/../' )
 from config_paths import config
 conf = config()
+
 import spacepy.coordinates as sc
 from spacepy.time import Ticktock
 
 runTester = False
 
 # units
-deg=np.pi/180
-amin=deg/60.
-hr=1.
-minn=hr/60.
-s=minn/60.
+deg = np.pi/180
+amin = deg/60.
+hr = 1.
+minn = hr/60.
+s = minn/60.
 
-#function to convert coordinate vector in MAG coordinates to one in GSM coordinates
-def MAGtoGSM(v_MAG,Time,ctype_in,ctype_out): # Time = [year,month,day,hours,minutes,seconds]
-    if( (ctype_in != 'sph' and ctype_in != 'car') or (ctype_out != 'sph' and ctype_out != 'car') ):
+
+def MAGtoGSM(v_MAG, Time, ctype_in, ctype_out): # Time = [year,month,day,hours,minutes,seconds]
+    '''Convert from MAG to GSM coordinates using SpacePy library'''
+    
+    if ( (ctype_in != 'sph' and ctype_in != 'car') or (ctype_out != 'sph' and ctype_out != 'car') ):
         print 'ctype ERROR'
-        return (np.nan)*np.empty((3,))
+        return (np.nan)*np.empty((3, ))
     v_MAG = np.array(v_MAG)
     cvals = sc.Coords(v_MAG, 'MAG', ctype_in)
-    T=tuple(Time)
+    T = tuple(Time)
     t_str = '%04d-%02d-%02dT%02d:%02d:%02d' % T # (year,month,day,hours,minutes,seconds)
     cvals.ticks = Ticktock(t_str, 'ISO') # add ticks
     newcoord = cvals.convert('GSM', ctype_out)
@@ -93,3 +98,8 @@ def UTtoHMS(UT):
     if(hours>=24):
         print 'WARNING: MIGHT BE NEXT DAY'
     return [hours,minutes,seconds]
+
+
+if __name__ == "__main__":
+    MAGtoGSM(v_MAG, Time, ctype_in, ctype_out)
+
