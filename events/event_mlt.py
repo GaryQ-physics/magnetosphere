5e453d743d90126c878d950efb2351866d14f6d5
Robert Weigel <rweigel@gmu.edu>
2020-05-27 12:16:15 -0400

    fix mlt script

--- events/event_mlt.py
+++ events/event_mlt.py
@@ -1,73 +1,43 @@
-# magnetic_local_time (MLT)
-
-#directory where magnetosphere folder is
-m_path = '/home/gary/'
-
+# Add MLT column to LOCALIZED.txt
 
 import sys
 import os
-import numpy as np
-sys.path.append( os.path.dirname(os.path.abspath(__file__)) + '/../' )
+
+sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '/../' )
 from config_paths import config
 conf = config()
-#sys.path.append(conf["m_path"] + 'magnetosphere/data/')
+
 import pos_sun as ps
 
-path = conf["m_path"] + 'magnetosphere/data/'
-fname = 'LOCALIZED_MLT.txt'
-N=50
+infile = conf["run_path_derived"] + 'LOCALIZED.txt'
+outfile = conf["run_path_derived"] + 'LOCALIZED_MLT.txt'
 
-# units
-deg = (np.pi/180.)
-amin = deg/60.
-hr = 1.
-minn = hr/60.
-s = minn/60.
+print('Opening ' + infile)
+g = open(infile,'r')
+lines = g.readlines()
+print('Opened ' + infile)
+g.close()
 
-g = open(path+'LOCALIZED.txt','r')
-f = open(path+fname,'w')
-print('writing ' + fname)
-print(g.readline()) # prints header line
-line=0
-for n in range(N):
-    line=line+1
-    # get string of the nth line of data
-    line_string = g.readline()
-    # get list of numbers from string  
-    line_list = [float(i) for i in line_string.split()] # line_list "=" [year,month,day,hours,minutes,mlon,mlat]
+print('Computing MLT for {0:d} events'.format(len(lines)))
+newlines = []
+for line in lines:    
+    
+    if line[0] == "#": # Header line
+        newlines.append(line.rstrip() + "   MLT\n")
+        continue    
+    
+    line_list = [float(i) for i in line.split()]
 
     # get data
-    Time = [line_list[0], line_list[1], line_list[2], line_list[3], line_list[4], 0.]
+    time = [line_list[0], line_list[1], line_list[2], line_list[3], line_list[4], 0.]
     MLONdeg = line_list[5]
-    MLATdeg=line_list[6]
-    phi=MLONdeg*deg
-    #theta=np.pi/2. - MLAT*deg
+    MLATdeg = line_list[6]
 
-    subsol_pt = ps.GSMtoMAG([1,0,0],Time,'car','car')
-    phi0 = np.arctan2(subsol_pt[1], subsol_pt[0])
-    
-    delta = phi-phi0
-    if(delta > np.pi):
-        delta=delta-2.*np.pi
-    elif(delta <= -np.pi):
-        delta=delta+2.*np.pi
-    MLT = 12.*hr + delta*24.*hr/(360*deg)
+    MLT = ps.MLTfromMAG(MLONdeg, time)
 
-    #x=np.cos(phi)*np.sin(theta)
-    #y=np.sin(phi)*np.sin(theta)
-    #z=np.cos(theta)
+    newlines.append(line.rstrip() + " {0:.2f}\n".format(MLT))
 
-    print "line ", line
-    print(line_list)
-    print(MLT)
-    #print(subsol_pt)
-    #print "GSM: ", ps.MAGtoGSM([x,y,z],month,day,year,UT)
-    #print(delta)
-    print Time
-    print "Mdip", ps.MAGtoGSM([0.,0.,1.],Time,'car','car')
-    # write to out file
-    f.write(str(MLT/hr) + '\n')
-
-g.close()
-f.close()
-print('wrote ' + fname)
+f = open(outfile, 'w')
+print('Writing ' + outfile)
+f.writelines(newlines)
+print('Wrote ' + outfile)
