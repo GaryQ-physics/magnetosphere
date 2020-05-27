70b745749e6de5c1c2262c3239fe73b2968e8739
GaryQ-physics <garyquaresima@gmail.com>
2020-05-27 12:13:01 -0400

    write vtk loop

--- vtk/write_VTKs.py
+++ vtk/write_VTKs.py
@@ -10,16 +10,33 @@ import field_line_vtk_script
 import kameleon_structured_grid_write
 import cut_plane
 
-#MLONdeg = 68.50
-#MLATdeg = 50.00
-#Event = [2003, 11, 20, 7, 0, 0, 68.50, 50.00]
+dpath = conf["m_path"] + 'magnetosphere/data/' #!!!!!!!!!!!!!!!!!
+N = 10
 
-Event = [2003, 11, 20, 7, 0, 0, 241.00, 55.00] # [year,month,day,hours,minutes,seconds,MLONdeg,MLATdeg]
+g = open(dpath + 'LOCALIZED.txt', 'r')
+print(g.readline()) # prints header line
+line = 0
+for n in range(N):
+    line = line + 1
+    # get string of the nth line of data
+    line_string = g.readline()
+    # get list of numbers from string  
+    line_list = [float(i) for i in line_string.split()] # line_list "=" [year,month,day,hours,minutes,mlon,mlat]
 
-cut_plane.writevtk(Event)
-field_line_vtk_script.writevtk(Event)
-kameleon_structured_grid_write.writevtk(Event,'p')
-kameleon_structured_grid_write.writevtk(Event,'jy')
-kameleon_structured_grid_write.writevtk(Event,'dB_dV')
-kameleon_structured_grid_write.writevtk(Event,'dBlon_dV')
-kameleon_structured_grid_write.writevtk(Event,'dBlat_dV')
+    # get data
+    time = line_list[0:5] + [0.]
+    Event = time + line_list[5:7]
+    T = tuple(time)
+    filename = conf["f_path"] + '3d__var_3_e' + '%04d%02d%02d-%02d%02d%02d-000' % T + '.out.cdf'
+    if os.path.exists(filename):
+        cut_plane.writevtk(Event)
+        field_line_vtk_script.writevtk(Event)
+        kameleon_structured_grid_write.writevtk(Event, 'p')
+        kameleon_structured_grid_write.writevtk(Event, 'jy')
+        kameleon_structured_grid_write.writevtk(Event, 'dB_dV')
+        kameleon_structured_grid_write.writevtk(Event, 'dBlon_dV')
+        kameleon_structured_grid_write.writevtk(Event, 'dBlat_dV')
+    else:
+        print('ERROR: ' + filename + ' does not exitst')
+
+g.close()
