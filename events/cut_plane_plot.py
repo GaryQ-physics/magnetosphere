964175bd6b3ab0be152498478e1c8f869aa7ffed
GaryQ-physics <garyquaresima@gmail.com>
2020-05-27 15:57:37 -0400

    make plot funct

--- events/cut_plane_plot.py
+++ events/cut_plane_plot.py
@@ -31,12 +31,11 @@ debug = False
 n=50 # number of pts on cutplane grid 
 m=50
 # parameter to plot
-parameter =  'p'
+#parameter =  'p'
 
 # Start point of main field line
-mlon = 68.50
-mlat = 50.00
-r = 1.01
+#mlon = 68.50
+#mlat = 50.00
 
 # Inputs for file and magnetic field model
 year = 2003
@@ -46,123 +45,132 @@ hours = 7
 minutes = 0
 seconds = 0
 
+xlim = [0,4]
+ylim = [-3,3]
 
-#def plot(Event, parameter, xlim=xlim, ylim=ylim, nx=n, ny=m, png=True):
 
 def data_in_U(kam, interp, variable, u, v, U1, U2, U3):
     # Get the data in the U coordinates (defined by the cut plane vectors U1 and U2)
-    x,y,z = u*U1+v*U2
-    B = np.array([ex_data(kam,interp,'bx', x,y,z), 
-                  ex_data(kam,interp,'by', x,y,z), 
-                  ex_data(kam,interp,'bz', x,y,z)])
+    x,y,z = u*U1 + v*U2
+    B = np.array([ex_data(kam, interp, 'bx', x, y, z), 
+                  ex_data(kam, interp, 'by', x, y, z), 
+                  ex_data(kam, interp, 'bz', x, y, z)])
     if variable == 'bu1':
-        return np.dot(B,U1)
+        return np.dot(B, U1)
     if variable == 'bu2':
-        return np.dot(B,U2)
+        return np.dot(B, U2)
     if variable == 'bu3':
-        return np.dot(B,U3)
+        return np.dot(B, U3)
     else:
-        return ex_data(kam,interp, variable, x,y,z)
+        return ex_data(kam, interp, variable, x, y, z)
 
 
-time = [year, month, day, hours, minutes, seconds]
 
-# Plot title
-title = 'SCARR5 ' + str(year) + '-' + str(month) + '-' + str(day) + 'T07:00'
-title = title + "\n" + "[mlat,mlon]=[{0:.1f}, {1:.1f}]".format(mlat, mlon)
+def plot(Event, parameter, xlim=xlim, ylim=ylim, nx=n, ny=m, png=True):
+    #Event = [year, month, day, hours, minutes, seconds, MLONdeg, MLATdeg]
+    time = Event[0:6]
+    mlon = Event[6]
+    mlat = Event[7]
 
-filename = conf["run_path"] + '3d__var_3_e' \
-            + '%04d%02d%02d-%02d%02d%02d-000' % tuple(time)
+    # Plot title
+    title = 'SCARR5 ' + str(year) + '-' + str(month) + '-' + str(day) + 'T07:00'
+    title = title + "\n" + "[mlat,mlon]=[{0:.1f}, {1:.1f}]".format(mlat, mlon)
 
-filename_in = filename + '.out.cdf'
-filename_out = filename + '.png'
+    filename = conf["run_path"] + '3d__var_3_e' \
+                + '%04d%02d%02d-%02d%02d%02d-000' % tuple(time)
 
-X0 = ps.MAGtoGSM([r, mlat, mlon], time, 'sph', 'car')
+    filename_in = filename + '.out.cdf'
+    filename_out = filename + '.png'
 
-# open kameleon
-kameleon = ccmc.Kameleon()
-print("Opening " + filename_in)
-kameleon.open(filename_in)
-print("Opened " + filename_in)
-interpolator = kameleon.createNewInterpolator()
+    r = 1.01
+    X0 = ps.MAGtoGSM([r, mlat, mlon], time, 'sph', 'car')
 
-parameter_unit = kameleon.getVisUnit(parameter)
+    # open kameleon
+    kameleon = ccmc.Kameleon()
+    print("Opening " + filename_in)
+    kameleon.open(filename_in)
+    print("Opened " + filename_in)
+    interpolator = kameleon.createNewInterpolator()
 
-# Trace field line
-s_grid = np.linspace(0., 10., 100)
-soln = odeint(dXds, X0, s_grid, args=(kameleon, interpolator))
-if debug:
-    print(X0)
-    print(np.dot(X0,X0))
-    print(soln)
+    parameter_unit = kameleon.getVisUnit(parameter)
 
-# initialize vectors for defining field line cut plane
-v1=(np.nan)*np.empty((3,))
-v2=(np.nan)*np.empty((3,))
-v3=(np.nan)*np.empty((3,))
-U1=(np.nan)*np.empty((3,))
-U2=(np.nan)*np.empty((3,))
-U3=(np.nan)*np.empty((3,))
-Mdipole=(np.nan)*np.empty((3,))
+    # Trace field line
+    s_grid = np.linspace(0., 10., 100)
+    soln = odeint(dXds, X0, s_grid, args=(kameleon, interpolator))
+    if debug:
+        print(X0)
+        print(np.dot(X0, X0))
+        print(soln)
 
-# define restriction condition on the field line points
-tr = np.logical_and(soln[:,0]**2 + soln[:,1]**2 + soln[:,2]**2 >= 1.,
-                    soln[:,0]**2 + soln[:,1]**2 + soln[:,2]**2 < 20.)
+    # initialize vectors for defining field line cut plane
+    v1 = (np.nan)*np.empty((3, ))
+    v2 = (np.nan)*np.empty((3, ))
+    v3 = (np.nan)*np.empty((3, ))
+    U1 = (np.nan)*np.empty((3, ))
+    U2 = (np.nan)*np.empty((3, ))
+    U3 = (np.nan)*np.empty((3, ))
+    Mdipole = (np.nan)*np.empty((3, ))
 
-# restricted field lines
-sol = soln[tr,:]
+    # define restriction condition on the field line points
+    tr = np.logical_and(soln[:, 0]**2 + soln[:, 1]**2 + soln[:, 2]**2 >= 1.,
+                        soln[:, 0]**2 + soln[:, 1]**2 + soln[:, 2]**2 < 20.)
 
-# define vects for plane of main field line
-v1 = sol[0,:]  # First point on field line
-v2 = sol[-1,:] # Last point on field line
-half = int(sol.shape[0]/2) 
-v3 = sol[half,:] # Approximate mid-point on field line
+    # restricted field lines
+    sol = soln[tr, :]
 
-# define cut plane coordinates based on main field line 
-# (U3 is normal to the plane)
-U2 = (v1-v2)/np.linalg.norm(v1-v2)
-Mdipole = ps.MAGtoGSM([0., 0., 1.], time, 'car', 'car')
-U3 = np.cross(v3-v1, U2)/np.linalg.norm(np.cross(v3-v1, U2))
-U1 = np.cross(U2, U3)
+    # define vects for plane of main field line
+    v1 = sol[0, :]  # First point on field line
+    v2 = sol[-1, :] # Last point on field line
+    half = int(sol.shape[0]/2) 
+    v3 = sol[half, :] # Approximate mid-point on field line
 
-x_1d = np.linspace(0, 4, n)
-y_1d = np.linspace(-3, 3, m)
-X, Y = np.meshgrid(x_1d, y_1d) # grid of points on the cutplane
-Z = np.zeros((n, m))
-for i in range(n):
-    for j in range(m):
-        # grid of the corresponding values of variable. To be color plotted
-        Z[i,j] = data_in_U(kameleon, interpolator, parameter, X[i,j], Y[i,j], U1, U2, U3)
+    # define cut plane coordinates based on main field line 
+    # (U3 is normal to the plane)
+    U2 = (v1-v2)/np.linalg.norm(v1-v2)
+    Mdipole = ps.MAGtoGSM([0., 0., 1.], time, 'car', 'car')
+    U3 = np.cross(v3-v1, U2)/np.linalg.norm(np.cross(v3-v1, U2))
+    U1 = np.cross(U2, U3)
 
-kameleon.close()
-print("Closed " + filename_in + "\n")
-print('Close plot to exit')
+    x_1d = np.linspace(xlim[0], xlim[1], n)
+    y_1d = np.linspace(ylim[0], ylim[1], m)
+    X, Y = np.meshgrid(x_1d, y_1d) # grid of points on the cutplane
+    Z = np.zeros((n, m))
+    for i in range(n):
+        for j in range(m):
+            # grid of the corresponding values of variable. To be color plotted
+            Z[i, j] = data_in_U(kameleon, interpolator, parameter, X[i, j], Y[i, j], U1, U2, U3)
 
-# Plotting
-plt.clf()
-fig = plt.figure(dpi=200)
-ax2 = fig.add_subplot(1,2,2)
+    kameleon.close()
+    print("Closed " + filename_in + "\n")
+    print('Close plot to exit')
 
-# Plot cut plane data
-ax2.set(title=title)
-ax2.set(xlabel="Tailward distance [$R_E$]")
-ax2.set(ylabel="Northward distance [$R_E$]")
-ax2.axis('square')
-pcm = ax2.pcolormesh(X, Y, Z)
+    # Plotting
+    plt.clf()
+    fig = plt.figure(dpi = 200)
+    ax2 = fig.add_subplot(1, 2, 2)
 
-# Reason for choice of fraction and pad:
-# https://stackoverflow.com/a/39948312/1491619
-cb = fig.colorbar(pcm, ax=ax2, fraction=0.04, pad=0.08)
-cb.ax.set_title(parameter + ' [' + parameter_unit + ']', ha='left')
-plt.xlim(0, 4)
-plt.ylim(-3, 3)
+    # Plot cut plane data
+    ax2.set(title = title)
+    ax2.set(xlabel = "Tailward distance [$R_E$]")
+    ax2.set(ylabel = "Northward distance [$R_E$]")
+    ax2.axis('square')
+    pcm = ax2.pcolormesh(X, Y, Z)
 
-# Add field line to 2D plot        
-solCut=np.zeros((sol.shape[0],2))
-for k in range(sol.shape[0]):
-    solCut[k,0]=np.dot(sol[k,:],U1)
-    solCut[k,1]=np.dot(sol[k,:],U2)
-ax2.plot(solCut[:,0],solCut[:,1], 'red', lw=1)
+    # Reason for choice of fraction and pad:
+    # https://stackoverflow.com/a/39948312/1491619
+    cb = fig.colorbar(pcm, ax = ax2, fraction = 0.04, pad = 0.08)
+    cb.ax.set_title(parameter + ' [' + parameter_unit + ']', ha='left')
+    plt.xlim(xlim[0], xlim[1])
+    plt.ylim(ylim[0], ylim[1])
+
+    # Add field line to 2D plot        
+    solCut=np.zeros((sol.shape[0], 2))
+    for k in range(sol.shape[0]):
+        solCut[k, 0] = np.dot(sol[k, :], U1)
+        solCut[k, 1] = np.dot(sol[k, :], U2)
+    ax2.plot(solCut[:, 0], solCut[:, 1], 'red', lw = 1)
 
 
-plt.show()
+    plt.show()
+
+plot([2003, 11, 20, 7, 0, 0, 68.50, 50.00],'p')
