plot titles and fixing units

vtk/field_line_vtk_script.py
@@ -17,7 +17,7 @@ import _CCMC as ccmc
 Nlong = 5
 Nb = 6
 sign = -1  # changes sign of magnetic field used to trace the field lines
-debug = False
+debug = True
 
 # units
 deg = (np.pi/180.)

vtk/kameleon_structured_grid_write.py
@@ -31,9 +31,10 @@ s = minn/60.
 A = 1e+6*muA
 Tesla = 1e+9*nT
 m = R_e/6.3781e+6  # R_e == 6.3781e+6*m
-kg = Tesla*A*s**2
+#kg = Tesla*A*s**2
 
-mu0 = 1.2566370614e-6*kg*m/((s**2)*(A**2))
+#mu0 = 1.2566370614e-6*kg*m/((s**2)*(A**2))
+mu0 = 1.970237314e-10*nT*R_e/muA
 
 def ex_data(kam,interp, variable, x,y,z, X0, Npole, V_char = 1.):
     if np.sqrt(x**2+y**2+z**2)<1e-4: return 0.
@@ -45,26 +46,30 @@ def ex_data(kam,interp, variable, x,y,z, X0, Npole, V_char = 1.):
                       ex_data(kam, interp, 'jz', x, y, z, X0, Npole)])
         J = J*(muA/m**2)
         R = np.array([x, y, z])-X0
-        R = R*m
-        B = (mu0/(4*np.pi))*np.cross(J, R)/(np.linalg.norm(R)**3)
-        dBnT = B*V_char/(nT)
-        if(variable=='dB_dV'): return np.linalg.norm(dBnT)
+        #R = R*R_e
+        #dB_dV = (mu0/(4*np.pi))*np.cross(J, R)/(np.linalg.norm(R)**3)
+        #dBnT = dB_dV*V_char/(nT)
+        dBnT = V_char*(mu0/(4*np.pi))*np.cross(J, R)/(np.linalg.norm(R)**3)
+        if(variable=='dB'): return np.linalg.norm(dBnT)
         a2 = np.cross(Npole, X0)
-        if(variable=='dBlon_dV'): return np.dot(dBnT, a2)/np.linalg.norm(a2)
+        if(variable=='dBx'): return np.dot(dBnT, a2)/np.linalg.norm(a2) # east west direction (east positive)
         a1 = np.cross(X0, a2)
-        if(variable=='dBlat_dV'): return np.dot(dBnT, a1)/np.linalg.norm(a1)
+        if(variable=='dBy'): return np.dot(dBnT, a1)/np.linalg.norm(a1) # north south direction (north positive)
     kam.loadVariable(variable)
     data = interp.interpolate(variable, x, y, z)
     return data
 
 def Compute(Event, var):
-    year,month,day,hours,minutes,seconds,MLONdeg,MLATdeg = Event
-    time = [year,month,day,hours,minutes,seconds]
-    X0 = ps.MAGtoGSM([1.,MLATdeg,MLONdeg],time,'sph','car')
+    #year,month,day,hours,minutes,seconds,MLONdeg,MLATdeg = Event
+    time = Event[0:6]
+    MLON = Event[6]
+    MLAT = Event[7]
+
+    X0 = ps.MAGtoGSM([1.,MLAT,MLON],time,'sph','car')
     Npole = ps.GEOtoGSM([0.,0.,1.],time,'car','car')
 
     # datafile
-    filename = conf["run_path"] + '3d__var_3_e' + '%04d%02d%02d-%02d%02d%02d-000' % (year,month,day,hours,minutes,seconds) + '.out.cdf'
+    filename = conf["run_path"] + '3d__var_3_e' + '%04d%02d%02d-%02d%02d%02d-000' % tuple(time) + '.out.cdf'
 
     # open kameleon ---------------
     kameleon = ccmc.Kameleon()
@@ -99,8 +104,8 @@ def Compute(Event, var):
 
 def writevtk(Event, var):
     A,B = Compute(Event,var)
-    year,month,day,hours,minutes,seconds,MLONdeg,MLATdeg = Event
-    tag = '_%04d:%02d:%02dT%02d:%02d:%02d' % (year,month,day,hours,minutes,seconds)
+    time = Event[0:6]
+    tag = '_%04d:%02d:%02dT%02d:%02d:%02d' % tuple(time)
     fname = conf["run_path_derived"] + 'kameleon_structured_grid_' + var + tag + '.vtk'
 
     Nx=Nx_main+Nx_tail