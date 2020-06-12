c2f44ecdc44d0816330604663cc1508aadc7a0dc
GaryQ-physics <garyquaresima@gmail.com>
2020-06-12 09:54:39 -0400

    trying to get biot savart to work

--- vtk/structured_grid_write.py
+++ vtk/structured_grid_write.py
@@ -6,32 +6,20 @@ sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '/../' )
 from config import conf
 
 import _CCMC as ccmc
+from units_and_constants import phys
 import pos_sun as ps
 from cut_plane import ex_data
 import biot_savart as bs
+from biot_savart_demo1 import J_kunits
+
 # run parameters
 
-dx = 0.5  # 0.2
-dy = 0.5
-dz = 0.5
+dx = .5
+dy = .5 # 0.2
+dz = .5
 dx_tail = 10.  # dx_tail not used if using bs.make_grid
-
-# units
-hr = 1.
-muA = 1.
-R_e = 1.
-nT = 1.
-
-deg = (np.pi/180.)
-amin = deg/60.
-minn = hr/60.
-s = minn/60.
-
-A = 1e+6*muA
-Tesla = 1e+9*nT
-m = R_e/6.3781e+6  # R_e == 6.3781e+6*m  (note m < 10^-6 Re)
-#kg = Tesla*A*s**2
-
+Test = False
+debug = False
 
 def J_kameleon(kam, interp, X):
     Jkunits = (np.nan)*np.empty(X.shape)
@@ -61,8 +49,12 @@ def Compute(Event, var, calcTotal=False):
     MLON = Event[7]
     MLAT = Event[8]
 
-    X0 = ps.MAGtoGSM([1.,MLAT,MLON],time[0:6],'sph','car')
-    Npole = ps.GEOtoGSM([0.,0.,1.],time[0:6],'car','car')
+    if Test:
+        X0 = np.array([0., 0.75, 0.])
+        Npole = np.array([0., 0., 1.])
+    else:
+        X0 = ps.MAGtoGSM([1.,MLAT,MLON],time[0:6],'sph','car')
+        Npole = ps.GEOtoGSM([0.,0.,1.],time[0:6],'car','car')
 
     # datafile
     filename = conf["run_path"] + '3d__var_3_e' + '%04d%02d%02d-%02d%02d%02d-%03d' % tuple(time) + '.out.cdf'
@@ -91,11 +83,14 @@ def Compute(Event, var, calcTotal=False):
     B3 = B3.flatten(order='C')
     Bgrid = np.column_stack((B1, B2, B3))
     '''
+    #tail = -200.
+    tail = -100.
 
-    ret = bs.make_grid([-200.,-20.], [-10., 10.], [-10., 10.], dx, dy, dz)
+    ret = bs.make_grid([tail, 15.], [-10., 10.], [-10., 10.], dx, dy, dz)
     Xgrid = ret[0]
-
-    if 'dB' in var:
+    
+    print('X0=',X0)
+    if 'dB' in var or var=='J':
         unit_v = np.array([1., 0., 0.])
         if '_EW' in var:
             a2 = np.cross(Npole, X0)
@@ -104,12 +99,26 @@ def Compute(Event, var, calcTotal=False):
             a2 = a2/np.linalg.norm(a2)
             unit_v = a2
         unit_v = np.repeat([unit_v], Xgrid.shape[0], axis=0)
+        
+        if Test:
+            Jin = J_kunits(Xgrid)*(phys['muA']/phys['m']**2)
+        else:
+            Jin = J_kameleon(kameleon, interpolator, Xgrid)*(phys['muA']/phys['m']**2)
 
-        deltaBnT = bs.deltaB('dB', X0, Xgrid, J=J_kameleon(kameleon, interpolator, Xgrid)*(muA/m**2)) #/nT
-        Aa = np.einsum('ij,ij->i', deltaBnT, unit_v) #https://stackoverflow.com/questions/15616742/vectorized-way-of-calculating-row-wise-dot-product-two-matrices-with-scipy
+        if debug:
+            print('unit_v=',unit_v)
+            print('Jin=',Jin)
+            print('Jin sum=', np.sum(Jin))
 
+        if var=='J':
+            Aa = Jin
+        else:
+            deltaBnT = bs.deltaB('dB', X0, Xgrid, Jin, V_char = dx*dy*dz) #/nT
+            Aa = np.einsum('ij,ij->i', deltaBnT, unit_v) #https://stackoverflow.com/questions/15616742/vectorized-way-of-calculating-row-wise-dot-product-two-matrices-with-scipy
+        if debug:
+            print('Aa=',Aa)
     else:
-        Aa = (np.nan)*np.empty((B1.size, ))
+        Aa = (np.nan)*np.empty((Xgrid.shape[0], ))
         for l in range(Aa.size):
             Aa[l] = ex_data(kameleon, interpolator, var, Xgrid[l, 0], Xgrid[l, 1], Xgrid[l, 2])
 
@@ -119,6 +128,8 @@ def Compute(Event, var, calcTotal=False):
     #-------------------------------
 
     if calcTotal:
+        print('Test=',Test)
+        print('dx,dy,dz = ', dx, dy, dz)
         print('total = ', np.sum(Aa))
         print(Xgrid.shape)
         print(Aa.shape)
@@ -133,7 +144,8 @@ def writevtk(Event, var, calcTotal=False):
     if not os.path.exists(conf["run_path_derived"] + subdir):
         os.mkdir(conf["run_path_derived"] + subdir)
 
-    print('also Nx = ',Nx)
+    if debug:
+        print('also Nx = ',Nx)
 
     f = open(fname,'w')
     print("Writing " + fname)
@@ -146,8 +158,11 @@ def writevtk(Event, var, calcTotal=False):
     np.savetxt(f, B)
     f.write('\n')
     f.write('POINT_DATA ' + str(Nx*Ny*Nz) + '\n')
-    f.write('SCALARS '+ var +' float 1\n')
-    f.write('LOOKUP_TABLE default\n')
+    if var=='J':
+        f.write('VECTORS ' + var + ' float\n')
+    else:
+        f.write('SCALARS ' + var + ' float 1\n')
+        f.write('LOOKUP_TABLE default\n')
     np.savetxt(f, Aa)
     f.close()
     print("Wrote " + fname)
