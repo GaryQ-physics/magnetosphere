b5419abe8c74a7ac3681c93b3a94018c18e56f0b
Robert Weigel <rweigel@gmu.edu>
2020-06-12 14:09:37 -0400

    kamodo test

--- misc/kamodo_test.py
+++ misc/kamodo_test.py
@@ -1,27 +1,25 @@
 # Dependencies:
+# Python 3.7
 # pip install kamodo
 # pip install execnet
 
 from kamodo.readers._kameleon_kamodo import Kameleon
 import numpy as np
 
-#python_path = '/Users/robertweigel/kameleon/bin/python'
-python_path = '/home/gary/magnetosphere/kameleon/bin/python'
-#python_path = '/usr/bin/python2' DOESNT WORK
+python_path = '/Users/robertweigel/kameleon/bin/python'
+fname = '/Users/robertweigel/magnetosphere/data/SCARR5_GM_IO2/IO2/3d__var_3_e20031120-070102-000.out.cdf'
 
+kameleon = Kameleon(fname, python_path, 'p')
 
-#fname = '/Users/robertweigel/magnetosphere/data/SCARR5_GM_IO2/IO2/3d__var_3_e20031120-070102-000.out.cdf'
-fname = '/home/gary/magnetosphere/data/SCARR5_GM_IO2/IO2/3d__var_3_e20031120-070000-000.out.cdf'
+points = np.linspace(2,10,999999).reshape((-1,3))
 
-kameleon = Kameleon(fname, python_path, 'rho') # "rho", "p", "bx", "by","bz")
-
-points = np.linspace(2,10,9999999).reshape((-1,3))
-
-print(points)
 print(points.shape)
 
 # Interpolate rho onto points.
-a = kameleon.rho(points)
-
-print(a)
-print(a.shape)
+import time
+to = time.time()
+data = kameleon.p(points)
+tf = time.time()
+print('{0:d} points in {1:.1f} s = {2:.1f} pts/s' \
+      .format(data.shape[0], tf-to, data.shape[0]/(tf-to))) 
+    
