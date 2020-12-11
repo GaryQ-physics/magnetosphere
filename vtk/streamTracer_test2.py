e073381af491745b4e3e13aedc1d98a8ced1467c
GaryQ-physics <garyquaresima@gmail.com>
2020-12-11 13:39:27 -0500

    trace function update

--- vtk/streamTracer_test2.py
+++ vtk/streamTracer_test2.py
@@ -58,6 +58,7 @@ traceFile (no callable option provided)
 		Uses triangulate to create grid
 
 """
+
 '''
 trace(x,y,z,bx,by,bz,points,method='scipy',callable=None)
 trace(x,y,z,bx,by,bz,points,method='vtk',callable=None)
@@ -114,7 +115,193 @@ trace(IC, Field, region=None, method='scipy')
 
 '''
 
-def trace(IC, Field, region=None, method='scipy', debug=True):
+def traceFile(filename, IC, method='vtk', debug=True):
+    ext = filename[-4:]
+
+    if method == 'scipy'
+        if ext == '.out':
+            import spacepy.pybats.bats as bats
+            data3d = bats.Bats2d(filename)
+
+            X = np.nan*np.empty((data3d['x'].shape,3), dtype=float) # makes dtype float64
+            X[:,0] = data3d['x']
+            X[:,1] = data3d['y']
+            X[:,2] = data3d['z']
+
+            B = np.nan*np.empty(X.shape, dtype=float) # makes dtype float64
+            B[:,0] = data3d['bx']
+            B[:,1] = data3d['by']
+            B[:,2] = data3d['bz']
+
+            return trace(X, B, IC, method=method, debug=debug)
+        elif ext == '.cdf':
+            pass
+        else:
+            raise ValueError
+
+    elif method == 'vtk':
+        if ext == '.vtk':
+            # read data
+            reader = vtk.vtkStructuredGridReader()
+            reader.SetFileName(Field)
+            reader.Update()
+            trace(None, reader, IC, method=method, debug=debug)
+        elif ext == '.out':
+            pass # how
+        elif ext == '.cdf':
+            pass # how
+        else:
+            raise ValueError
+
+    elif method == 'kameleon':
+        if ext!='.cdf' : raise ValueError
+        pass #copied
+
+
+
+
+
+def trace(Space, Field, IC, method='scipy', debug=True):
+    import types
+    sign = +1
+
+    if method=='scipy':
+        from scipy.integrate import odeint
+        from scipy.interpolate import RegularGridInterpolator, NearestNDInterpolator
+
+        if isinstance(Field, types.FunctionType):
+            def dXds(X, s):
+                F = Field(X)
+                Fmag = np.linalg.norm(F)
+                if 1e-9 < Fmag < 1e+7:
+                    return (sign/Fmag)*F
+                return [0., 0., 0.]
+
+            s_grid = np.arange(0., 10., 0.1)
+            max_iterations = 100
+
+            if IC.shape == (3,):
+                IC = [IC]
+            ret = []
+            linenum = 0
+            for X0 in list(IC):
+                if debug:
+                    print('linenum = ' + str(linenum))
+                done = False
+                solns = np.empty((0, 3)) # Combined solutions
+                i = 0
+                while not done:
+                    if debug:
+                        print('i = ' + str(i))
+                    soln = odeint(dXds, X0, s_grid)
+                    R = soln[:, 0]**2+soln[:, 1]**2 + soln[:, 2]**2
+                    # define condition on the field line points
+                    # Find first location where soln steps out-of-bounds
+                    #tr = np.where( False == (R >= 1) & (soln[:,0] > -30.) & (np.abs(soln[:, 2]) < 20.) )        
+                    # Boolean array.
+
+
+                    tr = (R >= 1) & (soln[:,0] > -30.) & (np.abs(soln[:, 2]) < 20.)
+                    # RuntimeWarning: invalid value encountered in greater_equal
+
+
+                    # Indices where stop conditions satisfied
+                    tr_out = np.where(tr == False)
+                    if debug:
+                        print(tr)
+                    if tr_out[0].size > 0:
+                        # Stop condition found at least once. Use solution up to that point.s
+                        solns = np.vstack((solns, soln[0:tr_out[0][0] + 1, :]))
+                        done = True
+                    elif max_iterations == i + 1:
+                        solns = np.vstack((solns, soln))   # return soln   faster?
+                        done = True
+                    else:
+                        # New initial condition is stop point.
+                        X0 = soln[-1, :]
+                        # Append solution but exclude last value, which is the
+                        # new initial condition.
+                        solns = np.vstack((solns, soln[0:-1, :]))
+                    i = i + 1
+                ret.append(solns)
+                linenum += 1
+
+            if len(ret) == 1:
+                return ret[0]
+            return ret
+
+        else:
+            Space = np.array(Space)
+            Field = np.array(Field)
+
+            if Space.shape == (3,3):
+                xlims = 
+                ...
+
+                from make_grid import make_axes
+                ax_list = make_axes(xlims,ylims,zlims,d)
+
+                # https://stackoverflow.com/questions/21836067/interpolate-3d-volume-with-numpy-and-or-scipy
+                Fx_interp = RegularGridInterpolator(tuple(ax_list), Field[0, :,:,:])
+                Fy_interp = RegularGridInterpolator(tuple(ax_list), Field[1, :,:,:])
+                Fz_interp = RegularGridInterpolator(tuple(ax_list), Field[2, :,:,:])
+
+                def Fcallable(v):
+                    return np.array([Fx_interp(v)[0], Fy_interp(v)[0], Fz_interp(v)[0]])
+
+            else:
+                assert(Space.shape == Field.shape)
+                if Space.shape[0]==3 and Space.shape[1]!=3:
+                    Space = Space.transpose()
+                    Field = Field.transpose()
+
+                Fx = NearestNDInterpolator(Space,Field[:,0])
+                Fy = NearestNDInterpolator(Space,Field[:,0])
+                Fz = NearestNDInterpolator(Space,Field[:,0])
+
+                def Fcallable(v):
+                    return np.array([Fx_interp(v)[0], Fy_interp(v)[0], Fz_interp(v)[0]])
+
+            return trace(None, Fcallable, IC, method=method, debug=debug)
+
+
+    elif method=='vtk':
+        assert(isinstance(Field,vtk.vtkObject)
+        print(Field)
+        print(type(Field))
+
+
+        rk = vtk.vtkRungeKutta45()
+        # Create source for streamtubes
+        streamer = vtk.vtkStreamTracer()
+        streamer.SetInputConnection(Field.GetOutputPort())
+        streamer.SetStartPosition(X[0],X[1],X[2])#SetSourceData()
+        streamer.SetMaximumPropagation(20) ###
+        #streamer.SetIntegrationStepUnit(2) # apperars overiden by next lines, see https://vtk.org/doc/nightly/html/classvtkStreamTracer.html#afe365e81e110f354065f5adc8401d589
+        streamer.SetMinimumIntegrationStep(0.01)
+        streamer.SetMaximumIntegrationStep(0.1)
+        streamer.SetInitialIntegrationStep(0.01)
+        #streamer.SetIntegrationDirection()
+        #streamer.SetIntegrationDirectionToForward()
+        #streamer.SetIntegrationDirectionToBackward()
+        streamer.SetIntegrationDirectionToBoth()
+        streamer.SetIntegrator(rk)
+        streamer.SetRotationScale(0.5)
+        streamer.SetMaximumError(1.0e-8)
+
+        #https://stackoverflow.com/questions/38504907/reading-a-vtk-polydata-file-and-converting-it-into-numpy-array
+        ## either order works ##
+        polydata = streamer.GetOutput()
+        streamer.Update()
+
+        return dsa.WrapDataObject(polydata).Points
+
+    else: raise ValueError
+
+
+
+
+def _trace(IC, Field, region=None, method='scipy', debug=True):
     ''' Field = file.npy, file.vtk, file.cdf, tkObject, foo, 
        method = 'scipy','vtk'  '''
     import types
@@ -406,7 +593,7 @@ def main():
 #vtkAlgorithmOutput
 
 if __name__=='__main__':
-    X = np.array([0.1,2.1,0.5])
+    IC = np.array([0.1,2.1,0.5])
     if False:
         main()
         arr = trace_old(X, view=False, write=True)
@@ -425,7 +612,7 @@ if __name__=='__main__':
 
     #ret = trace(X, foo, region=None, method='scipy')
     #ret = trace(X, cdffile, region=reg, method='scipy')
-    ret = trace(X, '/tmp/tmp.vtk', region=None, method='vtk')
+    ret = trace(None, '/tmp/tmp.vtk', IC, method='vtk')
 
     print('###################################')
     print(ret)
