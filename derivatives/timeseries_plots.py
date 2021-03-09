379c1bf666063e7fcd75a3410aabcc9b3fce1865
Gary Quaresima <garyquaresima@gmail.com>
2021-03-09 02:42:51 -0500

    better timeseries

--- derivatives/timeseries_plots.py
+++ derivatives/timeseries_plots.py
@@ -11,66 +11,57 @@ import util
 
 run = 'DIPTSUR2'
 n_times = None
-epsilons = [1./16., 1./8., 1./4., 1./2.]
+
+epsilons = [1./16., 1./8., 1./4., 1./2., 1., 2., 4., 8.]
 
 times = list(util.get_available_slices(run)[1])
 if n_times is not None:
     times = times[:n_times]
 
-averages = np.empty((len(epsilons), len(times))); averages[:,:]=np.nan
-integrals = np.empty(len(times)); integrals[:]=np.nan
-dtimes = []
+keys = []
+keys.append('integral')
+keys.append('absintegral')
+keys.append('num_total')
+for epsilon in epsilons:
+    keys.append('sum_epsilon_%f'%(epsilon))
+    keys.append('abssum_epsilon_%f'%(epsilon))
+    keys.append('num_epsilon_%f'%(epsilon))
+
+timeseries = pd.DataFrame(columns=keys, index=range(len(times)))
+
 for i in range(len(times)):
-    if (i%10 == 0): print('i=%d'%(i))
+    if i%10 == 0: print('i=%d'%(i))
     time = times[i]
     direct = conf[run+'_derived'] + 'derivatives/native_grid/'
     fname_df = direct + '%.2d%.2d%.2dT%.2d%.2d%.2d_df.pkl'%util.tpad(time, length=6)
     fname_meta = direct + '%.2d%.2d%.2dT%.2d%.2d%.2d_meta.txt'%util.tpad(time, length=6)
 
-    with open(fname_meta,'r') as handle:
-        meta = {}
-        for line in handle.readlines():
-            key, value = line.split(' ')
-            meta[key]=int(value)
+    #with open(fname_meta,'r') as handle:
+    #    meta = {}
+    #    for line in handle.readlines():
+    #        key, value = line.split(' ')
+    #        meta[key]=int(value)
 
     df = pd.read_pickle(fname_df)
 
-    tmparr = np.array(df['div_b1']*df['gridspacing'])
-    tmparr = np.nan_to_num(tmparr)
-    tmparr = np.abs(tmparr)
-    integrals[i] = np.sum(tmparr)
+    tmp = df['div_b1']*df['gridspacing']
+    tmp = tmp[tmp!=np.nan]
+    timeseries['integral'][i] = np.sum(tmp)
+    timeseries['absintegral'][i] = np.sum(np.abs(tmp))
+    timeseries['num_total'][i] = tmp.size
 
-    for j in range(len(epsilons)):
-        tmp = df['div_b1'][df['gridspacing'] == epsilons[j]]
+    for epsilon in epsilons:
+        tmp = df['div_b1'][df['gridspacing'] == epsilon]
         tmp = tmp[tmp!=np.nan]
-        summ = np.sum(tmp)
-        assert(len(tmp.shape)==1)
-        nn = tmp.size
-        averages[j,i] = summ/nn
+        timeseries['sum_epsilon_%f'%(epsilon)][i] = np.sum(tmp)
+        timeseries['abssum_epsilon_%f'%(epsilon)][i] = np.sum(np.abs(tmp))
+        timeseries['num_epsilon_%f'%(epsilon)][i] = tmp.size
 
-np.save("averages.npy",averages)
-np.save("integrals.npy",averages)
+timeseries.to_pickle('timeseries.pkl')
+
+
 
 dtimes = []
 for time in times:
     dtimes.append(datetime.datetime(time[0],time[1],time[2],time[3],time[4],time[5]))
 
-plt.plot(dtimes, integrals,'.',)
-plt.savefig('integrals.png')
-plt.clf()
-
-plt.plot(dtimes, averages[0,:],'.',)
-plt.savefig('averages0.png')
-plt.clf()
-
-plt.plot(dtimes, averages[1,:],'.',)
-plt.savefig('averages1.png')
-plt.clf()
-
-plt.plot(dtimes, averages[2,:],'.',)
-plt.savefig('averages2.png')
-plt.clf()
-
-plt.plot(dtimes, averages[3,:],'.',)
-plt.savefig('averages3.png')
-plt.clf()
